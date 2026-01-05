49e492ca9761e61dd9dade6d0f5ad1753e180ddf
sy2force <shayacoca@gmail.com>
2026-01-05 19:26:25 +0200

    fix: simplify vite config to resolve white screen (disable terser, enable sourcemaps)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -40,62 +40,15 @@ export default defineConfig({
     outDir: 'dist',
     emptyOutDir: true,
     assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'terser',
+    sourcemap: true,
     target: 'es2015',
-    chunkSizeWarningLimit: 500,
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
-        manualChunks: (id) => {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'vendor-react';
-            }
-            if (id.includes('react-router')) {
-              return 'vendor-router';
-            }
-            if (id.includes('framer-motion') || id.includes('@heroicons')) {
-              return 'vendor-ui';
-            }
-            if (id.includes('axios') || id.includes('react-hot-toast')) {
-              return 'vendor-utils';
-            }
-            if (id.includes('react-hook-form') || id.includes('yup')) {
-              return 'vendor-forms';
-            }
-            return 'vendor-misc';
-          }
-          if (id.includes('src/pages')) {
-            return 'pages';
-          }
-          if (id.includes('src/components')) {
-            return 'components';
-          }
-        },
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
-            return `assets/images/[name]-[hash][extname]`;
-          }
-          if (/css/i.test(ext)) {
-            return `assets/css/[name]-[hash][extname]`;
-          }
-          return `assets/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js'
-      }
-    },
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-        pure_funcs: ['console.log', 'console.info', 'console.debug', 'console.warn'],
-        passes: 2
-      },
-      mangle: {
-        safari10: true
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['framer-motion', '@heroicons/react']
+        }
       }
     }
   },
